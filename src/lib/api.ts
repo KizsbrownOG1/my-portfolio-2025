4911666495c194c0718d1f1d02a72b345b379077
KizsbrownOG1 <sponkibrownO.G@gmail.com>
2025-10-26 23:31:22 +0100

    redeploy

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,6 @@
-// src/utils/api.ts
 export const getApiUrl = () => {
+  if (window.location.hostname === "localhost") {
+    return "http://localhost:8000/send-mail.php";
+  }
   return "https://email-api.iceiy.com/send-mail.php";
 };
